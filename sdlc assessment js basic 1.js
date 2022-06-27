c4a755e784287396b885f08359866e4f7f0887d7
Rosenjah <107888511+Rosenjah@users.noreply.github.com>
2022-06-27 03:38:46 -0900

    Add files via upload

---
+++ sdlc assessment js basic 1.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you.
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysis = "This step involves breaking down the system in different pieces to analyze the situation, analyzing project goals, breaking down what needs to be created, and attempting to engage users so that definite requirements can be defined. ";
+const design = "The third phase describes, in detail, the necessary specifications, features and operations that will satisfy the functional requirements of the proposed system which will be in place.";
+const implementation = "This phase is when the majority of the code for the program is written. Additionally, this phase involves the actual installation of the newly-developed system. ";
+const testing = "Testers test the software against the requirements to make sure that the software is solving the needs addressed and outlined during the planning phase.";
+const maintain = "The final phase involves maintenance and regular required updates. This step is when end users can fine-tune the system, if they wish, to boost performance, add new capabilities or meet additional user requirements.";
